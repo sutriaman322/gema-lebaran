dbefdd23f06836ffbc96c0bf5131232daa7c2d6b
sutriaman <75231558+sutriaman322@users.noreply.github.com>
2026-04-02 21:52:21 +0700

    Refactor Vite config to use defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -26,3 +26,10 @@ export default defineConfig({
 })
 
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/gema-lebaran/'
+})
